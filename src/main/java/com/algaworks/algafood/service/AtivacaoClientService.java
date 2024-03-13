d86280afee65f8711d7fa8bf81df462567a24e77
AlineRibeiroBraga <aline_ribeirobraga@hotmail.com>
2024-03-12 23:55:34 -0300

    Ciclo de vida 3 - acessar as fases do ciclo de vida do blean com implmentação de interfaces

--- src/main/java/com/algaworks/algafood/service/AtivacaoClientService.java
+++ src/main/java/com/algaworks/algafood/service/AtivacaoClientService.java
@@ -4,14 +4,17 @@ import com.algaworks.algafood.modelo.Cliente;
 import com.algaworks.algafood.notificacao.NivelUrgencia;
 import com.algaworks.algafood.notificacao.Notificador;
 import com.algaworks.algafood.notificacao.TipoDoNotificador;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 //@Component
-public class AtivacaoClientService {
+public class AtivacaoClientService implements InitializingBean, DisposableBean {
 
     @TipoDoNotificador(value = NivelUrgencia.NORMAL)
     @Autowired
@@ -23,7 +26,7 @@ public class AtivacaoClientService {
     }
 
 //    @PreDestroy
-    public void destroy(){
+    public void destroy2(){
         System.out.println("DESTROY");
     }
 
@@ -31,5 +34,16 @@ public class AtivacaoClientService {
         cliente.ativar();
 
         notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+    }
+
+    //implementações das interfaces
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DESTROY");
     }
 }
