f5abe4def98b40a13034a0d207aeb8b4eebe42f8
AlineRibeiroBraga <aline_ribeirobraga@hotmail.com>
2024-03-14 23:48:54 -0300

    @ConfigurationProperties como criar uma classe com essa annotation

--- src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -1,7 +1,7 @@
 package com.algaworks.algafood.notificacao;
 
 import com.algaworks.algafood.modelo.Cliente;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +10,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificadorEmail implements Notificador {
 
-    @Value("${notificador.email.host-servidor}")
-    private String host;
-
-    @Value("${notificador.email.porta-servidor}")
-    private Integer porta;
+    @Autowired
+    private NotificadorProperties notificadorProperties;
 
     public NotificadorEmail() {
         System.out.println("NotificadorEmail REAL");
@@ -22,8 +19,8 @@ public class NotificadorEmail implements Notificador {
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.println("Host: " + host);
-        System.out.println("Porta: " + porta);
+        System.out.println("Host: " + notificadorProperties.getHostServidor());
+        System.out.println("Porta: " + notificadorProperties.getPortaServidor());
         System.out.printf("Notificando %s através do email %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
